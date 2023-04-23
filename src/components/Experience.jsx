1f8ba2efad832557f614790f8f72dfebec6eb029
Trushil <95337251+trushildhokiya@users.noreply.github.com>
2023-04-23 12:09:17 +0530

    completed experience section

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,10 +1,36 @@
 import React from 'react'
-import Journey from './Journey'
+import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeline-component';
+import 'react-vertical-timeline-component/style.min.css';
+import ExpData from '../static/data/ExpData';
+import '../static/styles/Experience.css';
 
 const Experience = () => {
   return (
-    <div id='experience' className='text-white'>
-      <Journey />
+    <div id='experience' className='p-1 lg:pl-[4rem] my-5'>
+      <h2 className='font-Lobster text-[4rem] text-amber-400 mb-[10rem]'>Experience</h2>
+      <div>
+        <VerticalTimeline>
+          {ExpData.map((element) => {
+            return (
+              <VerticalTimelineElement
+                contentStyle={{ background: 'rgb(2,90,78)', color: '#fff' }}
+                contentArrowStyle={{ borderRight: '7px solid  rgb(33, 150, 243)' }}
+                iconStyle={{ background: 'rgb(2,90,78)', color: '#fff' }}
+                date={element.date}
+                dateClassName='text-white'
+                visible={false}
+                icon={element.icon}
+              >
+                <h3 className="vertical-timeline-element-title font-Lato  pb-3 font-bold text-[1.5rem] timeline-title">{element.title}</h3>
+                <h4 className="vertical-timeline-element-subtitle font-bold  font-Nunito text-[0.8rem] timeline-subtitle">{element.subtitle}</h4>
+                <p className='font-Nunito text-xl text-justify'>
+                  {element.description}
+                </p>
+              </VerticalTimelineElement>
+            )
+          })}
+        </VerticalTimeline>
+      </div>
     </div>
   )
 }
---
+++ src/static/data/ExpData.js
@@ -0,0 +1,19 @@
+import {MdOutlineWorkHistory} from 'react-icons/md';
+const ExpData = [
+    {
+        title:"Technical Team Member",
+        subtitle:"Google Developer Students Club , KJSIT",
+        description:"I am currently working as Technical Team at G.D.S.C KJSIT. My role is to provide technical assistance to leads and working on projects",
+        date:"September 2022- present",
+        icon:<MdOutlineWorkHistory />,
+    },
+    {
+        title:"Joint Technical Head",
+        subtitle:"IET, KJSIT",
+        description:"I am currently working as Jt Heat at IET KJSIT, my role is to conduct various event,  managing and handling events ,providing technical assistance to team ,taking surveys",
+        date:"October 2022 - present",
+        icon:<MdOutlineWorkHistory />,
+    }
+]
+
+export default ExpData;
